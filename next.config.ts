b2c261972f349ee942f8da5333fc2e9089ee149d
al3xisvg <vasquezgarciaalexis@gmail.com>
2025-02-22 11:51:25 -0500

    [update] fix for build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone", // Exporta como HTML estático
   distDir: "out", // Carpeta donde se guardará el build
   images: { unoptimized: true }, // Deshabilita optimización de imágenes (Next.js usa una CDN propia)
 };
